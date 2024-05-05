4fae7b9d9bcb14e28cb341a43596c8ca5906be55
Bayu Setyantyuda <setyantyuda@gmail.com>
2024-05-06 02:51:29 +0700

    fix: main page layout

--- components/container/index.js
+++ components/container/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const Container = (props) => {
   return (
     <>
-      <div className="overflow-y-auto max-w-[1300px] mx-auto lg:px-10 md:px-14 px-6 lg:py-12 pt-10 pb-24">
+      <div className="overflow-y-auto max-w-[1100px] mx-auto lg:px-10 md:px-14 px-6 lg:py-12 pt-10 pb-24">
         {props.children}
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -54,67 +54,45 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <div className='flex overflow-hidden'>
-          {bannerData.map((item, i) => (
-            <div 
-              key={i} 
-              hidden={show !== i} 
-              className='h-[650px] w-full relative'
-            >
-              <img 
-                src={item?.image} 
-                alt='' 
-                className={`object-cover w-full h-full brightness-50 object-bottom ${isFull ? "scale-125 transition-all duration-[8000ms] ease-linear" : ""}`}
-              />
-              <div className='absolute w-full h-full top-0 bottom-0 left-0 right-0 z-20 mt-40'>
-                <Container>
-                  <div className={`w-[50%] space-y-12 leading-none absolute ${isFull ? "left-28" : "-left-full"} transition-all duration-500`}>
-                    <div className='text-white text-[54px] font-semibold'>{item?.title}</div>
-                    <div className='text-white text-xl'>{item?.desc}</div>
-                    <div className='flex space-x-3'>
-                      <Button title="Our Service"/>
-                      <Button title="Contact Us" contained/>
-                    </div>
+      <div className='flex h-[654px] relative'>
+        {bannerData?.map((item, i) => (
+          <div key={i} className={`absolute w-full h-[654px] ${show === i ? "left-0 blur-none" : "-left-full blur-md"} transition-all duration-700`}>
+            <img src={item.image} alt='' className={`object-cover brightness-50 w-full h-[650px] ease-linear object-bottom`}/>
+            <div className='absolute w-full h-full top-0 bottom-0 left-0 right-0 z-20 mt-40'>
+              <Container>
+                <div className={`w-[50%] space-y-12 leading-none absolute ${isFull ? "left-28" : "-left-full"} transition-all duration-500`}>
+                  <div className='text-white text-[54px] font-semibold'>{item?.title}</div>
+                  <div className='text-white text-xl'>{item?.desc}</div>
+                  <div className='flex space-x-3'>
+                    <Button title="Our Service"/>
+                    <Button title="Contact Us" contained/>
                   </div>
-                </Container>
-              </div>
-              <div className='absolute bottom-20 right-20 z-20 flex space-x-4'>
-                <ArrowBack
-                  fontSize='large' 
-                  className='hover:scale-150 scale-125 shadow-md hover:shadow-lg shadow-black bg-[#DBD5D2] p-2 transition-all duration-500 ease-in-out cursor-pointer'
-                />
-                <ArrowForward
-                  // onClick={nextHero}
-                  fontSize='large' 
-                  className='hover:scale-150 scale-125 shadow-md hover:shadow-lg shadow-black bg-[#DBD5D2] p-2 transition-all duration-500 ease-in-out cursor-pointer'
-                />
-              </div>
+                </div>
+              </Container>
             </div>
-          ))}
-        </div>
-        <div className='h-1 w-full flex'>
-          <div className={`bg-primary ${isFull ? "w-full transition-all duration-[8000ms] ease-linear" : "w-0"}`}></div>
-          <div className={`bg-transparent ${isFull ? "w-0" : "w-full"}`}></div>
+            <div className='h-1 w-full flex'>
+              <div className={`bg-primary ${isFull ? "w-full transition-all duration-[8000ms] ease-linear" : "w-0"}`}></div>
+              <div className={`bg-transparent ${isFull ? "w-0" : "w-full"}`}></div>
+            </div>
+          </div>
+        ))}
+        <div className='absolute bottom-20 right-20 z-20 flex space-x-4'>
+          <ArrowBack
+            fontSize='large' 
+            className='hover:scale-150 scale-125 shadow-md hover:shadow-lg shadow-black bg-[#DBD5D2] p-2 transition-all duration-500 ease-in-out cursor-pointer'
+          />
+          <ArrowForward
+            // onClick={nextHero}
+            fontSize='large' 
+            className='hover:scale-150 scale-125 shadow-md hover:shadow-lg shadow-black bg-[#DBD5D2] p-2 transition-all duration-500 ease-in-out cursor-pointer'
+          />
         </div>
       </div>
       <Container>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
-        <div>Hello ADPM</div>
+        <div className='space-y-5 py-16'>
+          <h2 className='font-bold text-secondary text-4xl'>Building Your Vision with Quality and Innovation</h2>
+          <p>Established in 2012, Struktura has been a leading force in the construction industry, specializing in commercial office building construction. Our commitment to quality and innovation sets us apart.</p>
+        </div>
       </Container>
     </>
   )
