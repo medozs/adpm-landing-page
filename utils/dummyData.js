463c0603787eea67b4529bf4174b6a52eb2bb45f
medozs <56469893+medozs@users.noreply.github.com>
2024-07-08 06:47:33 +0700

    dummy data

--- utils/dummyData.js
+++ utils/dummyData.js
@@ -16,24 +16,6 @@ export const bannerData = [
   },
 ]
 
-export const HeroBanner = [
-  {
-    image: "/17.png",
-    title: "Achieve Excellence",
-    desc: "Provide exceptional technical testing, inspection, and engineering design services with the highest standards to ensure safety and satisfaction in every project."
-  },
-  {
-    image: "/17.png",
-    title: "Drive Development",
-    desc: "Develop dynamic engineering design solutions tailored to the specific needs of each customer, driving project success with advanced technology."
-  },
-  {
-    image: "/17.png",
-    title: "Promote Integrity",
-    desc: "Uphold the highest standards of integrity and professionalism in every aspect of our work, fostering trust and long-term partnerships with our clients"
-  },
-]
-
 export const serviceList = [
     {
         id: 1,
